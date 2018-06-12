399885b47aa18b402ef0f8090f9b2d7ee77d2dad
Marko Milovanovic <mardevmil@gmail.com>
2018-06-12 18:42:45 +0200

    general exercise

---
+++ GeneralExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Testconsole.CalculationExercise;
+
+namespace GeneralExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Calculation calculation = new Calculation();
+           calculation.CalculateFactorial(10);
+           ConsoleKeyInfo keyinfo = Console.ReadKey();
+        }
+    }
+}
