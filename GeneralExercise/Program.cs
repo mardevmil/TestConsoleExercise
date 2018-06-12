68736c417f0dad2d8287c080cfdc0785609bff7f
Marko Milovanovic <mardevmil@gmail.com>
2018-06-12 19:31:19 +0200

    factorial changes

--- GeneralExercise/Program.cs
+++ GeneralExercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using Testconsole.CalculationExercise;
 
 namespace GeneralExercise
@@ -7,9 +8,38 @@ namespace GeneralExercise
     {
         static void Main(string[] args)
         {
-           Calculation calculation = new Calculation();
-           calculation.CalculateFactorial(10);
-           ConsoleKeyInfo keyinfo = Console.ReadKey();
+            Calculation calculation = new Calculation();                        
+            ConsoleKeyInfo keyinfo;
+            int n = 0;
+            Console.WriteLine("Please press some number!");
+            StringBuilder sb = new StringBuilder();
+            do
+            {
+                keyinfo = Console.ReadKey(true);
+
+                if (keyinfo.Key != ConsoleKey.Enter)
+                {
+                    if (Char.IsNumber(keyinfo.KeyChar))
+                    {
+                        Console.Write(keyinfo.KeyChar);
+                        sb.Append(keyinfo.KeyChar);
+                    }                                    
+                }                                  
+                else
+                {
+                    n = int.Parse(sb.ToString());
+                    Console.WriteLine("\nkeyChar: " + sb + " n: " + n + "\n");
+                    if (n > 0)
+                    {
+                        calculation.CalculateFactorial(n);
+                    }
+                    sb.Clear();
+                    Console.WriteLine("\n\n");
+                }                
+
+            }
+            while (keyinfo.Key != ConsoleKey.Spacebar);
+
         }
     }
 }
